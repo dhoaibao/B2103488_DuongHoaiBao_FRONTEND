f246809c149f81cc3bf557ec377d59fa023275f0
Duong Hoai Bao <dhbao2505@gmail.com>
2023-04-07 00:30:10 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,11 @@ const routes = [
         name: "contactbook",
         component: ContactBook,
     },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notfound",
+        component: () => import("@/views/NotFound.vue"),
+        },
 ];
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
