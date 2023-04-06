a397e1b5f681aa85d2a8a2ede23466bcf47d7e3f
Duong Hoai Bao <dhbao2505@gmail.com>
2023-04-07 00:25:41 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <script>
+import AppHeader from "@/components/AppHeader.vue";
 export default {
-}
+  components: {
+    AppHeader,
+  },
+};
 </script>
 <template>
-  <h1>Hello, Vue.js!</h1>
+  <div id="app">
+    <AppHeader />
+    <div class="container mt-3">
+      <router-view />
+    </div>
+  </div>
 </template>
 <style>
 .page {
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@fortawesome/fontawesome-free/js/all.min.js";
 
 import './assets/main.css'
 
 createApp(App).mount('#app')
+
+import router from "./router";
+createApp(App).use(router).mount("#app");
