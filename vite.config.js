92339c2dc99ffd03adff16325dc295f8aa2915ba
dhbao-dev <dhbao2505@gmail.com>
2024-02-22 20:10:12 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
   plugins: [vue()],
   resolve: {
     alias: {
---
+++ vue.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    publicPath: process.env.NODE_ENV === "production" ? "/B2103488_DuongHoaiBao_FRONTEND/" : "/",
+  };
+  
