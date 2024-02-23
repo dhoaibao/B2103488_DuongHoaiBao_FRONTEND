4c288e1346512efe0fc9bb4dba08d4283187ee34
dhbao-dev <dhbao2505@gmail.com>
2024-02-23 12:06:44 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/contactbook/",
+  base: "/B2103488_DuongHoaiBao_FRONTEND/",
   plugins: [vue()],
   resolve: {
     alias: {
