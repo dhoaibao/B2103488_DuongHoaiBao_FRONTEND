bc3167cdb94245967f7331fca576d9e2cea7c2d4
Dương Hoài Bảo <dhbao2505@gmail.com>
2024-02-24 13:34:11 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -14,12 +14,12 @@ export default defineConfig({
   },
   server: {
     port: 3001,
-    // proxy: {
-    //   "/api": {
-    //   // target: "http://localhost:3000/",
-    //   target: "https://b2103488-duong-hoai-bao-backend.vercel.app/",
-    //   changeOrigin: true,
-    //   },
-    //   }
+    proxy: {
+      "/api": {
+      // target: "http://localhost:3000/",
+      target: "https://b2103488-duonghoaibao-backend.onrender.com/",
+      changeOrigin: true,
+      },
+      }
     },
 })
